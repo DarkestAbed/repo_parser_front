e4bf4c565b2b819ea534ceeafddd98de8c22a759
Javier Tapia <javier.tapia@xepelin.com>
2023-08-19 09:53:41 -0400

    gitkeeps + refactor addrepo.js

--- scripts/addRepo.js
+++ scripts/addRepo.js
@@ -1,23 +1,33 @@
 const btn = document.querySelector("#add-btn");
 const form = document.querySelector("#add-repo");
+const url = form.querySelector("#url-input");
+const textInput = document.querySelector("#url-input");
 
-function getURL() {
-    let url = form.querySelector("#url-input").value;
+function getURL(url) {
     try {
         const newURL = new URL(url);
         console.log("URL es valida");
         console.log("GitHub repo path:", newURL.pathname.substring(1));
     } catch (error) {
         console.log(error); // => TypeError
+        alert("La URL no es un repo de GitHub válido");
     }
 };
 
 function sendData() {
-    // console.log("Testeando el click del botón!");
-    // alert("Testeando alertas con click!");
-    getURL();
+    const urlValue = url.value
+    if (urlValue === "") {
+        alert("El campo está vacío");
+    } else {
+        getURL(urlValue);
+    };
 };
+
+function reset(item) {
+    item.value = "";
+}
 
 btn.addEventListener("click", () => {
     sendData();
+    reset(textInput);
 });
